8b7296e8469da186acfdb22e3722e9a2a4064790
Saliheyp <m.saliheyupoglu@gmail.com>
2023-06-02 16:57:35 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+        }
+    }
+    enum Gunler{
+        Pazartesi,
+        Sali,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+}
